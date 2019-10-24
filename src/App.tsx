8424909c24d6051db0dd97a616aeefb6af618100
Jasen Pan <jasenp@ticketek.com.au>
2019-10-24 14:56:10 +1100

    update package

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
-import { Header, Footer } from "basic-test"
+import { Header, Footer, themeWithCustoms, MyTypography, ITheme } from "basic-test"
+import { ThemeProvider, makeStyles, createStyles } from "@material-ui/styles";
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(({ palette }: ITheme) => {
+  return createStyles({
+   heading1: {
+     backgroundColor: "#ff0000"
+   }
+  });
+});
 
 const App: React.FC = () => {
+  const classes = useStyles({});
   return (
+    <ThemeProvider theme={themeWithCustoms}>
     <div className="App">
       <Header foo="hello" bar="world" />
       <Footer name="hello" age="123"/>
+      <MyTypography variant="h1" className={classes.heading1}>abc123</MyTypography>
+      <Typography variant="h3" color="error">Im typography from outside</Typography>
     </div>
+    </ThemeProvider>
   );
 }
 
